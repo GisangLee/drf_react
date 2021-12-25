a909e77abbf91cbcbca75757f6919ea2618fbb1b
Dude <kisang6710@gmail.com>
2021-12-25 11:14:29 +0900

    change import url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import 'App.scss';
-import PostList from "PostList";
+import './App.scss';
+import PostList from "./PostList";
 
 function App() {
   return (
--- frontend/src/PostList.js
+++ frontend/src/PostList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import Post from "Post";
+import Post from "./Post";
 
 const API_URL = "http://127.0.0.1:8000/api/posts/";
 
